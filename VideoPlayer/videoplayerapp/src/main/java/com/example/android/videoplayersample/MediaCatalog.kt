daf60778d5284630023f158043d082817c86db43
feixiao <feixiao2020@sina.com>
2022-06-21 16:08:51 +0800

    update

--- VideoPlayer/videoplayerapp/src/main/java/com/example/android/videoplayersample/MediaCatalog.kt
+++ VideoPlayer/videoplayerapp/src/main/java/com/example/android/videoplayersample/MediaCatalog.kt
@@ -27,27 +27,27 @@ val mediaCatalog: List<MediaDescriptionCompat> = listOf(
             setDescription("MP4 loaded over HTTP")
             setMediaId("1")
             // License - https://peach.blender.org/download/
-            setMediaUri(Uri.parse("https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"))
+            setMediaUri(Uri.parse("https://172.20.99.13/tt.mp4"))
             setTitle("Short film Big Buck Bunny")
             setSubtitle("Streaming video")
             build()
-        },
-        with(MediaDescriptionCompat.Builder()) {
-            setDescription("MP4 loaded over HTTP")
-            setMediaId("2")
-            // License - https://archive.org/details/ElephantsDream
-            setMediaUri(Uri.parse("https://archive.org/download/ElephantsDream/ed_hd.mp4"))
-            setTitle("Short film Elephants Dream")
-            setSubtitle("Streaming video")
-            build()
-        },
-        with(MediaDescriptionCompat.Builder()) {
-            setDescription("MOV loaded over HTTP")
-            setMediaId("3")
-            // License - https://mango.blender.org/sharing/
-            setMediaUri(Uri.parse("https://ftp.nluug.nl/pub/graphics/blender/demo/movies/ToS/ToS-4k-1920.mov"))
-            setTitle("Short film Tears of Steel")
-            setSubtitle("Streaming audio")
-            build()
         }
+//        with(MediaDescriptionCompat.Builder()) {
+//            setDescription("MP4 loaded over HTTP")
+//            setMediaId("2")
+//            // License - https://archive.org/details/ElephantsDream
+//            setMediaUri(Uri.parse("https://archive.org/download/ElephantsDream/ed_hd.mp4"))
+//            setTitle("Short film Elephants Dream")
+//            setSubtitle("Streaming video")
+//            build()
+////        },
+//        with(MediaDescriptionCompat.Builder()) {
+//            setDescription("MOV loaded over HTTP")
+//            setMediaId("3")
+//            // License - https://mango.blender.org/sharing/
+//            setMediaUri(Uri.parse("https://ftp.nluug.nl/pub/graphics/blender/demo/movies/ToS/ToS-4k-1920.mov"))
+//            setTitle("Short film Tears of Steel")
+//            setSubtitle("Streaming audio")
+//            build()
+//        }
 )
